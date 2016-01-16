5ce82f70d2d2818f3ddcf2feac7d8057de0f04dd
Tyler Bevan <tdb506@york.ac.uk>
2016-01-16 16:09:52 +0000

    Generalise start and terminal symbols, add helper methods for inserting and annotating words

--- trie.py
+++ trie.py
@@ -8,19 +8,28 @@ class Node:
         self.descendants = {}  #Dictionary of descendants
         self.split_point = False
 
-    def __insert__(self, string, graph, last_key = '^'):
-        """If the first letter isn't already an immediate descendant of the node
-        then create a new node for that letter"""
+    def __insert__(self, string, graph, terminal, last_key = str()):
         letter = string[0]
 
-        if (letter != '^'):
-            if (letter == '$'):
+        """-------------------- GRAPH BUILDING --------------------"""
+        """If not first letter or start symbol"""
+        if (last_key != ''):
+            """Add node, colour code s for emphasis"""
+            if (letter == terminal):
                 graph.add_node(last_key + letter, label = letter, style = 'filled', fillcolor = 'pink')
             else:
                 graph.add_node(last_key + letter, label = letter)
+            """Add edge"""
             graph.add_edge(last_key, last_key + letter)
             last_key = last_key + letter
+        else:
+            """Add first node"""
+            graph.add_node(letter, label = letter)
+            last_key = letter
 
+        """-------------------- TRIE BUILDING --------------------"""
+        """If the first letter isn't already an immediate descendant of the node
+        then create a new node for that letter"""
         if letter not in self.descendants:
             self.descendants[letter] = Node(letter)
             """Trigger update procedure to check if split_point"""
@@ -30,7 +39,7 @@ class Node:
         """If there are remaining characters in the string"""
         if len(string[1:]) > 0:
             """Insert the remaining chunk of string below"""
-            self.descendants[letter].__insert__(string[1:], graph, last_key)
+            self.descendants[letter].__insert__(string[1:], graph, terminal, last_key)
 
     def __repr__(self):
         """Outputs the branching factor and the dictionary of descendants
@@ -38,16 +47,20 @@ class Node:
         return str(self.branching_factor) + " " + str(self.descendants)
 
 class Trie:
-    def __init__(self):
+    def __init__(self, start = str(), terminal = str()):
         self.root = Node()
-        self.graph = pgv.AGraph(directed=False)
-
+        self.graph = pgv.AGraph(directed=True)
+        self.start = start
+        self.terminal = terminal
 
     def __repr__(self):
         return str(self.root)
 
     def insert(self, string):
-        self.root.__insert__(string, self.graph)
+        self.root.__insert__(string, self.graph, self.terminal)
+
+    def insert_word(self, word):
+        self.insert(self.start + word + self.terminal)
 
     def annotate(self, string):
         node = self.root
@@ -62,7 +75,10 @@ class Trie:
                 return "String not in trie"
         return("Annotation of '" + string + "': " + annotation)
 
-    def contains_string(self, string):
+    def annotate_word(self, word):
+        return self.annotate(self.start + word + self.terminal)
+
+    def contains(self, string):
         node = self.root
         for letter in string:
             if letter in node.descendants:
@@ -72,7 +88,7 @@ class Trie:
         return True
 
     def contains_word(self, string):
-        return("Trie contains word '" + string + "': " + str(self.contains_string("^" + string + "$")))
+        return("Trie contains word '" + string + "': " + str(self.contains(self.start + string + self.terminal)))
 
     def draw_graph(self, png_name):
         print('Processing graph layout...')
@@ -82,14 +98,13 @@ class Trie:
         print("Graph '"  + png_name + ".png' successfully exported!")
 
 """-------------------------------- MAIN PROGRAM --------------------------------"""
-test_trie = Trie()
+test_trie = Trie(start = '^', terminal = '$')
 txt_in = 'wordlist'
 png_out = 'trie'
 
 with open(txt_in + '.txt') as file:
     for word in file:
-        test_trie.insert("^" + word.rstrip() + "$")
+        test_trie.insert_word(word.rstrip())
     file.close()
 
-print(test_trie.annotate("^aardvark$"))
 test_trie.draw_graph(png_out)
